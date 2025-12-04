e0268a915965cfe47213d14474f9e2542db001df
KirkWalker <kirk@kirkwalker.ca>
2025-12-03 22:05:43 -0800

    feat(chat): validate husky and eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,33 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    ...js.configs.recommended,
+  },
+];
